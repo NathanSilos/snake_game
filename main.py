c32593fc4ddb138aa1f8a42a6d028eee489aa631
NathanSilos <nathansilos@gmail.com>
2023-05-19 21:11:12 -0300

    Incluindo arquivo mp3, alteracao na velocidade do jogo que aumenta com o tempo, controle de pontos

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ import sys
 from functions import *
 import time
 import random
+import os
+import pandas as pd
 
+dir = os.path.dirname(os.path.abspath(__file__))
 # Initializing pygame, music and font
 pg.font.init()
 pg.font.init()
 pg.mixer.init()
 
 # Start the music
-music_player('C:\\Users\\kbadmin\\Documents\\Youtube\\audio\\Solomun-The-way-back.mp3')
+music_player(dir + '\\Solomun-The-way-back.mp3')
 
 # Create a screen
 screen = pg.display.set_mode((600, 400))
 
 # Import the screen image
-background = pg.image.load('C:\\Users\\kbadmin\\Documents\\Youtube\\audio\\snake-game.jpg')
+background = pg.image.load( dir + '\\snake_game_home.jpg')
 
 # Transform the image to the size of screen
 background = pg.transform.scale(background, (600, 400)) 
@@ -28,6 +31,7 @@ grey = (105,105,105) # Grey
 green = (34, 145, 43) # Green
 red = (209, 58, 54) # Red
 yellow = (255, 255, 0) # Yellow
+white = (229, 229, 224) # White
 
 # Defining positions variables
 xfood = random.randrange(1, 600, 1)
@@ -74,11 +78,25 @@ while True:
         screen.blit(background,(0,0))
 
         # Insert PLAY button
-        game_font = pg.font.Font('C:\\Users\\kbadmin\Desktop\pygame\\VCR_OSD_MONO_1.001.ttf', 40)
+        game_font = pg.font.Font( dir + '\\VCR_OSD_MONO_1.001.ttf', 40)
         play_button = pg.Rect((470, 300, 110, 60))
         play_button_font = game_font.render('PLAY', True, red)
         play_rect = play_button_font.get_rect()
+        
         screen.blit(play_button_font, (480, 311))
+        
+        # Ler o histórico
+        df = pd.read_csv(dir + '\\base_snake.csv')
+        # Ordena
+        df.sort_values(by=['pontos'], inplace=True, ascending=False)
+
+        max_score = df.pontos.max()
+
+        # Pontuacao máxima
+        max_point = game_font.render(f'Max points: {max_score}', True, white)
+        play_rect = max_point.get_rect()
+
+        screen.blit(max_point, (20, 360))
         
         # Getting the mouse position
         mouse = pg.mouse.get_pos()
@@ -115,7 +133,7 @@ while True:
             snake_body.pop(-1)
             # When the snake touchs it body
             for snake_position_body in snake_body[1:]:
-                print(snake_position_body[0], snake_head[0])
+                # print(snake_position_body[0], snake_head[0])
                 if (snake_position_body[0] > snake_head[0] - 5 and  snake_position_body[0] < snake_head[0] + 5) and\
                 (snake_position_body[1] > snake_head[1] - 5 and  snake_position_body[1] < snake_head[1] + 5):
                     # Returns to the initial page
@@ -124,7 +142,7 @@ while True:
             snake_body = [snake_head]
 
         # Draw a score in the left side of the screen
-        score_font = pg.font.Font('C:\\Users\\kbadmin\Desktop\pygame\\VCR_OSD_MONO_1.001.ttf', 20)
+        score_font = pg.font.Font(dir + '\\VCR_OSD_MONO_1.001.ttf', 20)
         score_counter = score_font.render(f'Score:{score}', True, black)
         screen.blit(score_counter, (10, 10))
 
@@ -134,9 +152,12 @@ while True:
         
         # Drawing the food 
         pg.draw.rect(screen, red,(xfood, yfood, 10, 10))
+        
+        # Setting the start speed
+        snake_speed = 12 
 
         # Snake speed
-        speed.tick(20)
+        speed.tick(snake_speed + score)
 
         # When the snake touch the food
         if (xsnake > xfood - 10 and  xsnake < xfood + 10) and (ysnake > yfood - 10 and  ysnake < yfood + 10):
@@ -152,6 +173,11 @@ while True:
         if (snake_head[0] < 0 or snake_head[0] > 600) or (snake_head[1] > 400 or snake_head[1] < 0):            
             xsnake = 300
             ysnake = 200
+
+            # Cria um df com o valor dos pontos
+            dfp = pd.DataFrame({'pontos': [score]})
+            # Concatena o outro valor e exporta
+            pd.concat([df, dfp]).to_csv(r'C:\Users\natha\Documents\01-Projetos\snake_game-main\base_snake.csv', index=False)
 
             # Returns to the initial page
             display_menu = True
